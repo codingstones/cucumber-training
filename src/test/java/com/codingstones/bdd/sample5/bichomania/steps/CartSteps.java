18287b8eca293ff7dd8f7d929e2926b12a42bc70
Dani Latorre <dani@danilat.com>
2017-04-11 01:59:27 +0200

    bichomania sample to page objects

--- src/test/java/com/codingstones/bdd/sample5/bichomania/steps/CartSteps.java
+++ src/test/java/com/codingstones/bdd/sample5/bichomania/steps/CartSteps.java
@@ -1,22 +1,22 @@
 package com.codingstones.bdd.sample5.bichomania.steps;
 
+import com.codingstones.bdd.sample5.bichomania.pages.Cart;
+import com.codingstones.bdd.sample5.bichomania.pages.Page;
+import com.codingstones.bdd.sample5.bichomania.pages.ProductDetail;
+import com.codingstones.bdd.sample5.bichomania.pages.ProductList;
 import com.codingstones.bdd.utils.BrowserDriver;
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CartSteps {
+    ProductList productList;
+    ProductDetail productDetail;
+    Cart cart;
 
     @After
     public void tearDown() {
@@ -26,33 +26,23 @@ public class CartSteps {
     @Given("^an empty cart$")
     public void an_empty_cart() throws Throwable {
         System.out.println("Well.. the cart is empty for each driver instance ;)");
-        BrowserDriver.getCurrentDriver().get("http://www.bichomania.es/cart");
-        List lineItems = BrowserDriver.getCurrentDriver().findElements(By.id("line_items"));
-        assertEquals(0, lineItems.size());
+        cart = new Page(BrowserDriver.getCurrentDriver()).goToCart();
+        assertTrue(cart.isEmpty());
     }
 
     @Given("^I am looking for a dog product$")
     public void i_am_looking_for_a_dog_product() throws Throwable {
-        BrowserDriver.getCurrentDriver().get("http://www.bichomania.es/t/categorias/perros");
-        WebElement productAnchor = BrowserDriver.getCurrentDriver().findElement(By.cssSelector("div.product-item a"));
-        productAnchor.click();
-
+        productList = new ProductList(BrowserDriver.getCurrentDriver());
+        productDetail = productList.goToAProduct();
     }
 
     @When("^I add the product to the cart$")
     public void i_add_the_product_to_the_cart() throws Throwable {
-        WebDriverWait wait = new WebDriverWait(BrowserDriver.getCurrentDriver(), 5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("add-to-cart-button")));
-        WebElement addToCart = BrowserDriver.getCurrentDriver().findElement(By.id("add-to-cart-button"));
-        addToCart.click();
+        cart = productDetail.addToCart();
     }
 
     @Then("^the product should be present in my cart$")
     public void the_product_should_be_present_in_my_cart() throws Throwable {
-        WebDriverWait wait = new WebDriverWait(BrowserDriver.getCurrentDriver(), 5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("line_items")));
-        WebElement lineItems = BrowserDriver.getCurrentDriver().findElement(By.id("line_items"));
-        List items = lineItems.findElements(By.className("line-item"));
-        assertEquals(1, items.size());
+        assertFalse(cart.isEmpty());
     }
 }
